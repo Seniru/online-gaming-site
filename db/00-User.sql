8bd4d205e57c41f37af5ef9293803d49347cc89c
Seniru <senirupasan@gmail.com>
2024-09-27 13:08:59 +0530

    Set up database files

---
+++ db/00-User.sql
@@ -0,0 +1,15 @@
+CREATE TABLE User (
+
+);
+
+CREATE TABLE ProUser (
+
+);
+
+CREATE TABLE DevUser (
+
+);
+
+CREATE TABLE SupportAgent (
+
+);
---
+++ db/01-Game.sql
@@ -0,0 +1,19 @@
+CREATE TABLE Game (
+
+);
+
+CREATE TABLE Comment (
+
+);
+
+CREATE TABLE  Category (
+
+);
+
+CREATE TABLE GameCategory (
+
+);
+
+CREATE TABLE UserComment (
+
+);
