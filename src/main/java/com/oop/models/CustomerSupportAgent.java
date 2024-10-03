ea0c9cc76bdd59366499638dc26872087c059c6e
Siyumika <164844686+Siyumika@users.noreply.github.com>
2024-10-03 11:14:53 +0000

    create CustomerSupportAgent

--- src/main/java/com/oop/models/CustomerSupportAgent.java
+++ src/main/java/com/oop/models/CustomerSupportAgent.java
@@ -1,5 +1,17 @@
 package com.oop.models;
 
 public class CustomerSupportAgent {
+
+	public CustomerSupportAgent(String username, String password, String email, String profilePicture)
+{
+	super(username,password,email,ProfilePicture);
+	    
+}
+
+public void resolveTicket(Ticket t)
+{
 	
+}
+	
+    
 }
