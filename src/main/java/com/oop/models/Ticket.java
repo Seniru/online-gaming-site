e82ce5a3f24d76cdb066559dbd2da1806cc65c50
supunsliit <it23209084@my.sliit.lk>
2024-10-03 11:43:29 +0000

    ticket

--- src/main/java/com/oop/models/Ticket.java
+++ src/main/java/com/oop/models/Ticket.java
@@ -7,4 +7,64 @@ public class Ticket {
 	    String Body;
 	    String Username;
 
+
+
+        public class Ticketw {
+	    
+		int TicketID;
+	    String Title;
+	    String Body;
+	    String Username;
+	    
+	    
+	  
+		public int getTicketID() {
+			return TicketID;
+		}
+		
+		public void setTicketID(int ticketID) {
+			TicketID = ticketID;
+		}
+
+
+		public String getTitle() {
+			return Title;
+		}
+
+		public void setTitle(String title) {
+			Title = title;
+		}
+
+
+		public String getBody() {
+			return Body;
+		}
+
+
+		public void setBody(String body) {
+			Body = body;
+		}
+
+
+		public String getUsername() {
+			return Username;
+		}
+
+		public void setUsername(String username) {
+			Username = username;
+		}
+
+
+		public Ticketw(int ticketID, String title, String body, String username) {
+			super();
+			TicketID = ticketID;
+			Title = title;
+			Body = body;
+			Username = username;
+		}
+	    
+	    
+}
+
+
 }
