e286e45b1e898c0e3d07df46754b2ee51ba45854
supunsliit <it23209084@my.sliit.lk>
2024-10-03 11:52:51 +0000

    category

--- src/main/java/com/oop/models/Category.java
+++ src/main/java/com/oop/models/Category.java
@@ -5,4 +5,35 @@ public class Category {
 	    String Cname;
 	    String Icon;
 	    String Color;
+
+	public Ticketw(String cname, String icon, String color) {
+		super();
+		Cname = cname;
+		Icon = icon;
+		Color = color;
+	}
+
+	public String getCname() {
+		return Cname;
+	}
+	public void setCname(String cname) {
+		Cname = cname;
+	}
+
+	public String getIcon() {
+		return Icon;
+	}
+
+	public void setIcon(String icon) {
+		Icon = icon;
+	}
+	public String getColor() {
+		return Color;
+	}
+
+	public void setColor(String color) {
+		Color = color;
+	}
+    
+}
 }
