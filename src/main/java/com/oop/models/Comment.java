cf144346e0bea6e34f8fa1dec2b4f2f38713734c
Siyumika <164844686+Siyumika@users.noreply.github.com>
2024-10-10 07:54:23 +0000

    create comment class

--- src/main/java/com/oop/models/Comment.java
+++ src/main/java/com/oop/models/Comment.java
@@ -4,6 +4,35 @@ import java.io.PrintWriter;
 
 public class Comment implements Persistable, Printable {
 
+    private int commentID;
+    private String gtitle;
+    private String body;
+
+    public Comment(int commentID, String gtitle, String body) {
+		
+		this.catommentID = commentID;
+		this.gtitle = gtitle;
+		this.body = body;
+	}
+    
+
+    public void setCommentID(int commentID) {
+		this.commentID = commentID;
+	}
+	public String getGtitle() {
+		return gtitle;
+	}
+	public void setGtitle(String gtitle) {
+		this.gtitle = gtitle;
+	}
+	public String getBody() {
+		return body;
+	}
+	public void setBody(String body) {
+		this.body = body;
+	}
+
+    
     @Override
     public void print(PrintWriter out) {
         // TODO Auto-generated method stub
